0f18d0c880fb47e645a9250863ee73fe4a293449
kuldeepdevda70 <kuldeepdevda134@gmail.com>
2025-01-05 19:22:46 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,8 @@ function App() {
    const dispatch=useDispatch()
    
      useEffect(()=>{
-      dispatch(checkAuth())
+        const token =JSON.parse(sessionStorage.getItem('token'))
+      dispatch(checkAuth(token))
      
      },[dispatch])
 
